7e19503413aa6387dbba915e8dfb9ae8825517d3
xintara <aintara1312@gmail.com>
2024-07-29 21:52:56 -0700

    initial mergesort implementation

--- visualizer/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ visualizer/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AlgorithmVisualizer.css';
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms.js';
 
 export default class AlgorithmVisualizer extends React.Component {
     constructor(props) {
@@ -16,10 +17,20 @@ export default class AlgorithmVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 100; i++) {
-            array.push(randomIntFromInterval(5, 1000));
+        for (let i = 0; i < 70; i++) {
+            array.push(randomIntFromInterval(5, 700));
         }
         this.setState({array});
+    }
+
+    mergeSort() {
+        const javaScriptSortedArray = this.state.array
+        .slice()
+        .sort((a, b) => a - b);
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+        console.log(sortedArray);
+
+        console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
     }
 
     render() {
@@ -27,14 +38,29 @@ export default class AlgorithmVisualizer extends React.Component {
         
         return (
             <>
-                {array.map((value, idx) => (
-                    <div className="array-bar" key={idx}>
-                        {value}
-                    </div>
-                ))}
+                <div className="array-container">
+                    {array.map((value, idx) => (
+                        <div 
+                        className="array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}></div>
+                    ))}
+                </div>
+                <div className="button-container">
+                    <button onClick={() => this.resetArray()}>Generate New Array</button>
+                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                </div>
             </>
         );
     }
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) return false
+    }
+    return true;
 }
 
 function randomIntFromInterval(min, max) {
